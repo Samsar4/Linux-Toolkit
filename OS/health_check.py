c7ee981bd049dd1d441d53dc7cc656ad30aa1fb2
Samsar4 <samsar4sec@gmail.com>
2021-02-16 22:47:44 +0000

    up

---
+++ OS/health_check.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+
+import psutil
+import shutil
+
+def check_disk_usage(disk):
+    du = shutil.disk_usage(disk)
+    free = du.free / du.total * 100
+    return free > 20
+    
+def check_cpu_usage():
+    usage = psutil.cpu_percent(1)
+    return usage < 75
+    
+if not check_disk_usage("/") or not check_cpu_usage():
+    print("Err!")
+else:
+    print("Everything ok")
+
